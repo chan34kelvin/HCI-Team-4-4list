c646ce866020356b526a8bc97bc66cc1b7567c8b
Ashwini Managuli <ashwinimanaguli@Ashwinis-MacBook-Pro.local>
2021-12-14 03:49:18 -0800

    Added changes to homepage

--- 4list/src/components/Home.js
+++ 4list/src/components/Home.js
@@ -1,3 +1,4 @@
+import { height } from 'dom-helpers';
 import React, {useState} from 'react';
 import { Card, CardGroup } from 'react-bootstrap';
 import Navbar from './Navbar';
@@ -6,109 +7,225 @@ import Navigation from './navigation/Navigation';
 const Home = () => {
 
     return (
-        <div className='m-4' class="scrollable">
+        <><div style={{ display: 'flex', flexDirection: 'row', marginLeft: 'auto', marginRight: 'auto'}}>
             {/* <Navigation/> */}
             {/* <Navbar/> */}
-            <CardGroup>
-            <>
-  <Card className='m-4' style={{ width: '25rem'}}>
- 
-   <Card.Header style={{backgroundColor: '#ff8c00'}}>For Sale</Card.Header>
-    <Card.Body>
-      <Card.Text>
-      <Card.Link href="#">Furniture</Card.Link>
-      </Card.Text>
-      <Card.Text>
-      <Card.Link href="#">Books</Card.Link>
-      </Card.Text>
-      <Card.Link href="#">Cars</Card.Link>
-      
-    </Card.Body>
-  </Card>
-  <br />
+            <br></br><br></br><br></br>
+            
+            <Card style={{ width: '15rem', height: '15rem', marginLeft: '20rem', marginRight: '5rem'}}>
 
-  <Card className='m-4' style={{ width: '25rem' }}>
-    <Card.Header style={{backgroundColor: '#ff8c00'}}>Personal</Card.Header>
-    <Card.Body>
-    <Card.Text>
-      <Card.Link href="#">Furniture</Card.Link>
-      </Card.Text>
-      <Card.Text>
-      <Card.Link href="#">Books</Card.Link>
-      </Card.Text>
-      <Card.Link href="#">Cars</Card.Link>
-    </Card.Body>
-  </Card>
-  <br />
+                <Card.Header style={{ backgroundColor: '#ff8c00', textTransform: 'uppercase'}}><Card.Title><Card.Link className='link-dark ms-2' style={{textUnderlinePosition: 'off'}} href="/forum">For Sale</Card.Link></Card.Title></Card.Header>
+                <Card.Body style={{overflow: 'auto' }}>
+                    <Card.Text style={{ textColor: '#00000' }}>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                    <Card.Text style={{ textColor: '#00000' }}>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                    <Card.Text style={{ textColor: '#00000' }}>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
 
-  <Card className='m-4' style={{ width: '25rem' }}>
-    <Card.Header style={{backgroundColor: '#ff8c00'}}>Housing</Card.Header>
-    <Card.Body>
-    <Card.Text>
-      <Card.Link href="#">Furniture</Card.Link>
-      </Card.Text>
-      <Card.Text>
-      <Card.Link href="#">Books</Card.Link>
-      </Card.Text>
-      <Card.Link href="#">Cars</Card.Link>
-    </Card.Body>
-  </Card>
-  <br />
 
-  
-</>
-            </CardGroup>
 
-            <br></br><br></br>
+                </Card.Body>
+            </Card>
+            <br />
 
-            <CardGroup>
-            <>
-  <Card className='m-4' style={{ width: '25rem' }}>
-    <Card.Header style={{backgroundColor: '#ff8c00'}}>Jobs</Card.Header>
-    <Card.Body>
-    <Card.Text>
-      <Card.Link href="#">Furniture</Card.Link>
-      </Card.Text>
-      <Card.Text>
-      <Card.Link href="#">Books</Card.Link>
-      </Card.Text>
-      <Card.Link href="#">Cars</Card.Link>
-    </Card.Body>
-  </Card>
-  <br />
+            <Card style={{ width: '15rem', height: '15rem', marginLeft: '5rem', marginRight: '5rem'}}>
+                <Card.Header style={{ backgroundColor: '#ff8c00', textTransform: 'uppercase' }}><Card.Title><Card.Link className='link-dark ms-2' style={{textUnderlinePosition: 'off'}} href="/forum">Personal</Card.Link></Card.Title></Card.Header>
+                <Card.Body style={{overflow: 'auto' }}>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br />
 
-  <Card className='m-4' style={{ width: '25rem' }}>
-    <Card.Header style={{backgroundColor: '#ff8c00'}}>Community</Card.Header>
-    <Card.Body style={{overflow:'auto'}}>
-    <Card.Text>
-      <Card.Link href="#">Furniture</Card.Link>
-      </Card.Text>
-      <Card.Text>
-      <Card.Link href="#">Books</Card.Link>
-      </Card.Text>
-      <Card.Link href="#">Cars</Card.Link>
-    </Card.Body>
-  </Card>
-  <br />
+            <Card style={{ width: '15rem', height: '15rem', marginLeft: '5rem', marginRight: '5rem' }}>
+                <Card.Header style={{ backgroundColor: '#ff8c00', textTransform: 'uppercase' }}><Card.Title><Card.Link className='link-dark ms-2' style={{textUnderlinePosition: 'off'}} href="/forum">Housing</Card.Link></Card.Title></Card.Header>
+                <Card.Body style={{overflow: 'auto' }}>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br />
 
-  <Card className='m-4' style={{ width: '25rem' }}>
-    <Card.Header style={{backgroundColor: '#ff8c00'}}>Events</Card.Header>
-    <Card.Body>
-    <Card.Text>
-      <Card.Link href="#">Furniture</Card.Link>
-      </Card.Text>
-      <Card.Text>
-      <Card.Link href="#">Books</Card.Link>
-      </Card.Text>
-      <Card.Link href="#">Cars</Card.Link>
-    </Card.Body>
-  </Card>
-  <br />
 
-  
-</>
-            </CardGroup>
-        </div>
+        </div><br></br><br></br><br></br><div style={{ display: 'flex', flexDirection: 'row', marginLeft: 'auto', marginRight: 'auto' }}>
+
+                        <Card style={{ width: '15rem', height: '15rem', marginLeft: '20rem', marginRight: '5rem'}}>
+                            <Card.Header style={{ backgroundColor: '#ff8c00', textTransform: 'uppercase' }}><Card.Title><Card.Link className='link-dark ms-2' style={{textUnderlinePosition: 'off'}} href="/forum">Jobs</Card.Link></Card.Title></Card.Header>
+                            <Card.Body style={{overflow: 'auto' }}>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                        <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                    </Card.Text>
+                    <Card.Text>
+                    <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                    </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <br />
+
+                        <Card style={{ width: '15rem', height: '15rem', marginLeft: '5rem', marginRight: '5rem' }}>
+                            <Card.Header style={{ backgroundColor: '#ff8c00', textTransform: 'uppercase' }}><Card.Title><Card.Link className='link-dark ms-2' style={{textUnderlinePosition: 'off'}} href="/forum">Community</Card.Link></Card.Title></Card.Header>
+                            <Card.Body style={{ overflow: 'auto' }}>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <br />
+                        <Card style={{ width: '15rem', height: '15rem', marginLeft: '5rem', marginRight: '5rem' }}>
+                            <Card.Header style={{ backgroundColor: '#ff8c00', textTransform: 'uppercase' }}><Card.Title><Card.Link className='link-dark ms-2' style={{textUnderlinePosition: 'off'}} href="/event">Events</Card.Link></Card.Title></Card.Header>
+                            <Card.Body style={{overflow: 'auto' }}>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Furniture</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                    <Card.Link className='link-dark ms-2' href="#">Books</Card.Link>
+                                </Card.Text>
+                                <Card.Text>
+                                <Card.Link className='link-dark ms-2' href="#">Cars</Card.Link>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <br />
+                        
+
+                        <footer style={{marginTop: '20rem', textAlign: 'center'}}>HCI Class SFSU CS Dept. Fall 2021 Team 4</footer>
+
+                  
+            </div></>
+
     );
 }
 
